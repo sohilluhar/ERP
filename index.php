ae47bff952a59e0586c507b2447c305f57ce6a0a
sohilluhar <sohilayubluhar@gmail.com>
2020-03-15 21:03:21 +0530

    Login required Added

--- index.php
+++ index.php
@@ -21,11 +21,10 @@
 
             <form action="./verifyuser.php" method="post">
                 <div class="input-group mb-3">
-                    <input type="email" name="email" class="form-control" placeholder="Email">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required>
                 </div>
                 <div class="input-group mb-3">
-                    <input type="password" name="password" class="form-control" placeholder="Password">
-
+                    <input type="password" name="password" class="form-control" placeholder="Password" required>
                 </div>
                 <div class="row">
 
